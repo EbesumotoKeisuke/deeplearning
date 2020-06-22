020a7530177d93a374fb378212050f9661968bf3
smile <smile.0627.e@docomo.ne.jp>
2020-06-22 19:39:26 +0900

    解析結果をファイル出力し、現在時刻等を記録できるように修正

--- OpenCV-CNN/Find-crack/Select-crack.py
+++ OpenCV-CNN/Find-crack/Select-crack.py
@@ -7,12 +7,15 @@ from keras.models import model_from_json
 from keras.preprocessing import image
 import numpy as np
 import sys
+from datetime import datetime
 
+# 結果保存先出力pathの指定
+output_path = "Output/Debug2000data/test1_FullData/"
 
 #保存したモデルの読み込み
-model = model_from_json(open('Output/Debug2000data/test2_EqualData/tea_predict.json').read())
+model = model_from_json(open(output_path + 'tea_predict.json').read())
 #保存した重みの読み込み
-model.load_weights('Output/Debug2000data/test2_EqualData/tea_predict.hdf5')
+model.load_weights(output_path + 'tea_predict.hdf5')
 
 # 識別種類
 categories = ["CD", "UD", "CP", "UP", "CW", "UW"]
@@ -28,24 +31,57 @@ x = np.expand_dims(x, axis=0)
 #予測
 features = model.predict(x)
 
-#予測結果によって処理を分ける
-if features[0, 0] == 1:
-    print ("This is the data with cracks in the bridge.")
+# 現在時刻の取得
+now_time = datetime.now()
 
-elif features[0, 1] == 1:
-    print ("This is the data without cracks in the bridge.")
+#結果の保存。予測結果によって処理を分ける
+with open(output_path + 'Analysis-Result.csv', 'a', encoding='utf-8') as f:
+    if features[0, 0] == 1:
+        f.write(str(now_time))
+        f.write(",")
+        f.write(img_path)
+        f.write(",This is the data with cracks in the bridge.\n")
+        print ("This is the data with cracks in the bridge.")
 
-elif features[0, 2] == 1:
-    print ("This is the data with paint cracks.")
+    elif features[0, 1] == 1:
+        f.write(str(now_time))
+        f.write(",")
+        f.write(img_path)
+        f.write(",This is the data without cracks in the bridge.\n")
+        print ("This is the data without cracks in the bridge.")
 
-elif features[0, 3] == 1:
-    print ("This is the data with no paint cracks.")
+    elif features[0, 2] == 1:
+        f.write(str(now_time))
+        f.write(",")
+        f.write(img_path)
+        f.write(",This is the data with paint cracks.\n")
+        print ("This is the data with paint cracks.")
 
-elif features[0, 4] == 1:
-    print ("This is data with concrete cracks.")
+    elif features[0, 3] == 1:
+        f.write(str(now_time))
+        f.write(",")
+        f.write(img_path)
+        f.write(",This is the data with no paint cracks.\n")
+        print ("This is the data with no paint cracks.")
 
-elif features[0, 5] == 1:
-    print ("This is the data without cracking of concrete.")
+    elif features[0, 4] == 1:
+        f.write(str(now_time))
+        f.write(",")
+        f.write(img_path)
+        f.write(",This is data with concrete cracks.\n")
+        print ("This is data with concrete cracks.")
 
-else:
-    print("I couldn't tell. Is it really a picture of a concrete wall?")
+    elif features[0, 5] == 1:
+        f.write(str(now_time))
+        f.write(",")
+        f.write(img_path)
+        f.write(",This is the data without cracking of concrete.\n")
+        print ("This is the data without cracking of concrete.")
+
+    else:
+        f.write(str(now_time))
+        f.write(",")
+        f.write(img_path)
+        f.write(",I couldn't tell. Is it really a picture of a concrete wall?\n")
+        print("I couldn't tell. Is it really a picture of a concrete wall?")
+f.close()
